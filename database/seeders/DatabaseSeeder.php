e4bbc0637c5c18b5160c261c3f3c1b485e53b498
sachilles <kina98@gmx.ch>
2023-03-01 16:45:18 +0100

    Product seeder Dataseeder angepasst

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,6 +20,11 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CountriesSeeder::class,
         ]);
+
+        Product::factory(10)->create()->each(function ($product) {
+            $product->categories()
+                ->saveMany(Category::factory(mt_rand(1, 2))->make());
+        });
 
         // \App\Models\User::factory(10)->create();
 
