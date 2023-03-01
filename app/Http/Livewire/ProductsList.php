6dcddb5885e61b15cb28aa25ed17f4f8c93d41ed
sachilles <kina98@gmx.ch>
2023-03-01 22:07:20 +0100

    Query gefixt. HeroIcons implementiert

--- app/Http/Livewire/ProductsList.php
+++ app/Http/Livewire/ProductsList.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Category;
+use App\Models\Country;
 use App\Models\Product;
+use Illuminate\View\View;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -10,11 +13,80 @@ class ProductsList extends Component
 {
     use WithPagination;
 
-    public function render()
+    public array $categories = [];
+
+    public array $countries = [];
+
+    public string $sortColumn = 'products.name';
+
+    public string $sortDirection = 'asc';
+
+    public array $searchColumns = [
+        'name' => '',
+        'price' => ['', ''],
+        'description' => '',
+        'category_id' => 0,
+        'country_id' => 0,
+    ];
+
+    public function mount(): void
     {
-        $products = Product::paginate(10);
-        return view('livewire.products-list', [
-            'products' => $products,
+        $this->categories = Category::pluck('name', 'id')->toArray();
+        $this->countries = Country::pluck('name', 'id')->toArray();
+    }
+
+    public function sortByColumn($column): void
+    {
+        if ($this->sortColumn == $column) {
+            $this->sortDirection = $this->sortDirection == 'asc' ? 'desc' : 'asc';
+        } else {
+            $this->reset('sortDirection');
+            $this->sortColumn = $column;
+        }
+    }
+
+    protected $queryString = [
+        'sortColumn' => [
+            'except' => 'products.name'
+        ],
+        'sortDirection' => [
+            'except' => 'asc',
+        ],
+    ];
+
+    public function render(): View
+    {
+        $products = Product::query()
+            ->select(['products.*', 'countries.id as countryId', 'countries.name as countryName',])
+            ->join('countries', 'countries.id', '=', 'products.country_id')
+            ->with('categories');
+
+        // Verwenden Sie diesen Code hier
+        foreach ($this->searchColumns as $column => $value) {
+            if (!empty($value)) {
+                $products->when($column == 'price', function ($products) use ($value) {
+                    if (is_numeric($value[0])) {
+                        $products->where('products.price', '>=', $value[0] * 100);
+                    }
+                    if (is_numeric($value[1])) {
+                        $products->where('products.price', '<=', $value[1] * 100);
+                    }
+                })
+                    ->when($column == 'category_id', function ($query) use ($value) {
+                        return $query->whereHas('categories', function ($query) use ($value) {
+                            return $query->where('categories.id', $value);
+                        });
+                    })
+                    ->when($column == 'country_id', fn($products) => $products->whereRelation('country', 'id', $value))
+                    ->when($column == 'name', fn($products) => $products->where('products.' . $column, 'LIKE', '%' . $value . '%'));
+            }
+        }
+
+        $products->orderBy($this->sortColumn, $this->sortDirection);
+
+        return view('livewire.products-list',  [
+            'products' => $products->paginate(10)
         ]);
     }
+
 }
